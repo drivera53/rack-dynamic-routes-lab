7cf2327f54c5d02c85f9516fe32b7b2d514587a9
Danny Rivera <samboard@hotmail.com>
2021-03-12 10:13:59 -0500

    commit tree b5cacf7008ac78c036aeea0653759888f682804a

---
+++ app/application.rb
@@ -0,0 +1,23 @@
+class Application
+    @@item = []
+
+    def call(env)
+      resp = Rack::Response.new
+      req = Rack::Request.new(env)
+  
+        if req.path.match(/items/)
+            item_title = req.path.split("/items/").last #turn /item/apple into apple
+            if item = @@items.find{|s| s.name == item_title}
+                resp.write item.price
+            else 
+                resp.status = 400
+                resp.write "Item not found"
+            end
+        else
+            resp.write "Route not found"
+            resp.status = 404
+        end
+  
+      resp.finish
+    end
+  end 
